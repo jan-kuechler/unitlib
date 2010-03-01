7c9aad99112097dad0bfb445111eec7d65e48f5f
Jan <kuchen_@gmx.de>
2010-03-01 18:44:42 +0100

    lua: Added parse(str) function, that returns a unit lua: Added tostring and equality support to the unit metatable

--- luabinding.c
+++ luabinding.c
@@ -4,21 +4,39 @@
 #include "unitlib.h"
 
 #define EXPORT __declspec(dllexport)
-
 #define UNUSED(var) do{(void)(var);}while(0)
+
+#define UNIT "unit"
+
+static void error(lua_State *L)
+{
+	lua_pushstring(L, ul_error());
+	lua_error(L);	//Note: lua_error does not return
+}
+
+static unit_t *create_unit(lua_State *L)
+{
+	unit_t *unit = lua_newuserdata(L, sizeof(*unit));
+
+	luaL_getmetatable(L, UNIT);
+	lua_setmetatable(L, -2);
+
+	return unit;
+}
 
 int l_test(lua_State *L)
 {
 	UNUSED(L);
-	printf("test\n");
+	ul_parse(NULL, NULL);
+	error(L);
 	return 0;
 }
 
 int l_init(lua_State *L)
 {
-	bool ok = ul_init();
-	lua_pushboolean(L, ok);
-	return 1;
+	if (!ul_init())
+		error(L);
+	return 0;
 }
 
 int l_quit(lua_State *L)
@@ -28,24 +46,58 @@ int l_quit(lua_State *L)
 	return 0;
 }
 
-int l_error(lua_State *L)
+int l_parse(lua_State *L)
 {
-	const char *msg = ul_error();
-	lua_pushstring(L, msg);
+	const char *str = luaL_checkstring(L, 1);
+	unit_t *u = create_unit(L);
+	if (!ul_parse(str, u)) {
+		error(L);
+	}
+	return 1;
+}
+int lm_tostring(lua_State *L)
+{
+	unit_t *unit = luaL_checkudata(L, 1, UNIT);
+
+	// TODO: Find something better than a static buffer
+	static char buffer[4096];
+	if (!ul_snprint(buffer, 4096, unit, UL_FMT_PLAIN, NULL))
+		error(L);
+
+	lua_pushstring(L, buffer);
+	return 1;
+}
+
+int lm_equal(lua_State *L)
+{
+	unit_t *a = luaL_checkudata(L, 1, UNIT);
+	unit_t *b = luaL_checkudata(L, 2, UNIT);
+
+	lua_pushboolean(L, ul_equal(a, b));
 	return 1;
 }
 
 EXPORT int luaopen_unitlib(lua_State *L)
 {
-	luaL_Reg reg[] = {
-		{"init", l_init},
-		{"quit", l_quit},
-		{"error", l_error},
-		{"test", l_test},
+	luaL_Reg lib[] = {
+		{"init",  l_init},
+		{"quit",  l_quit},
+		{"parse", l_parse},
+		{"test",  l_test},
 		{NULL, NULL},
 	};
 
-	luaL_register(L, "unitlib", reg);
+	luaL_Reg meta[] = {
+		{"__tostring", lm_tostring},
+		{"__eq", lm_equal},
+		{NULL, NULL},
+	};
 
-	return 0;
+	luaL_register(L, "unitlib", lib);
+
+	luaL_newmetatable(L, UNIT);
+	luaL_register(L, NULL, meta);
+	lua_pop(L, 1);
+
+	return 1;
 }
